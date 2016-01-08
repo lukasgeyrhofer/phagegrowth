2c4fd8094523a146a9850918592397381172be3e
lukasgeyrhofer <lukas.geyrhofer@gmx.at>
2016-01-08 15:50:18 +0100

    added code

--- phagegrowth.py
+++ phagegrowth.py
@@ -1,1 +1,170 @@
-/home/lukas/projects/phagegrowth/program/phagegrowth_btdistr_151212.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import argparse
+import math,sys
+import scipy
+
+
+def growthrate(nutrients):
+    #   function that reduces cell's growth rate with depletion of nutrients
+    #   Kmax/((1+Ks)/c_nutrients)
+    return args.bacteria_growth_Kmax*nutrients/(nutrients+args.bacteria_growth_Kc)
+    # np.array([args.bacteria_growth_Kmax/(1+(args.bacteria_growth_Kc/n)) if n>0 else 0 for n in nutrients])
+
+def burstsize(nutrients):
+    #   linear function that reduces phage burst size on susceptibles with respect to bacterial growth rate
+    return args.phage_burstsize_linear*growthrate(nutrients)+args.phage_burstsize_const
+
+
+def f(y,yd):
+  # unpack arrays
+  n,s,si,r,ri,p = y
+  spDELAY,rpDELAY = yd
+  # assume phage concentration is contant at boundaries
+  # shifted arrays necessary for diffusion term of phage movement
+  pfwd  = np.concatenate((p[1:],np.array([p[-1]])))
+  pback = np.concatenate((np.array([p[0]]),p[:-1]))
+  
+  gr = growthrate(n)
+
+  return np.array([	-gr*args.nutrients_per_cell*(s+r), \
+			gr*s - args.phage_bacteria_absorptionconstant*s*p, \
+			args.phage_bacteria_absorptionconstant *(s*p-spDELAY), \
+			gr*r - args.phage_bacteria_absorptionconstant*args.bacteria_resitant_absorptionreduction*p*r, \
+			args.phage_bacteria_absorptionconstant*args.bacteria_resitant_absorptionreduction*(r*p - rpDELAY) ,\
+			burstsize(n)*args.phage_bacteria_absorptionconstant*(spDELAY+args.bacteria_resitant_absorptionreduction*rpDELAY)-args.phage_bacteria_absorptionconstant*(s+si+r+ri)*p + args.phage_diffusionconstant/args.dx**2*(pback - 2*p + pfwd)])
+
+def RungeKutta4(y,yd):
+  # 4th order Runge-Kutta integration scheme
+  k1 = args.epsilon*f(y,yd[0])
+  k2 = args.epsilon*f(y+k1/2.,(yd[0]+yd[1])/2.)
+  k3 = args.epsilon*f(y+k2/2.,(yd[0]+yd[1])/2.)
+  k4 = args.epsilon*f(y+k3,yd[1])
+  ret = y + (k1+2*k2+2*k3+k4)/6.
+
+  if args.cutatzero:
+      ret[ret<0] = 0
+  else:
+    # restrict solutions for cells/phages to have more than one cell/phage in each bin
+    (ret[1:])[ret[1:]<1] = 0
+    # nutrient concentration should be larger than 0
+    (ret[0:])[ret[0:]<0] = 0
+  return ret
+
+def main():
+    parser = argparse.ArgumentParser()
+    
+    # different argument groups allow additional information to be printed to the screen
+    # in the end everything is still parsed to the same "args" variable
+    
+    # define lattice
+    parser_lattice = parser.add_argument_group(description="Lattice parameters")
+    parser_lattice.add_argument("-s","--space",type=int,default=200)
+    parser_lattice.add_argument("-d","--dx",type=float,default=1e-2)
+    
+    # algorithm parameters
+    parser_algorithm = parser.add_argument_group(description="Algorithm parameters")
+    parser_algorithm.add_argument("-e","--epsilon",type=float,default=5e-3) # measured in hrs, thus default eps=1/200h=0.3min
+    parser_algorithm.add_argument("-M","--maxsteps",type=int,default=5000)
+    parser_algorithm.add_argument("-O","--outputsteps",type=int,default=100)
+    parser_algorithm.add_argument("-o","--outputbins",type=int,default=1)
+    parser_algorithm.add_argument("-Q","--quiet",default=False,action="store_true")
+    parser_algorithm.add_argument("-Z","--cutatzero",default=False,action="store_true")
+
+    # interactions between various trophic levels
+    parser_interactions = parser.add_argument_group(description="Interactions between different trophic levels")
+    parser_interactions.add_argument("-D","--phage_diffusionconstant",type=float,default=6e-5)
+    parser_interactions.add_argument("-b","--phage_burstdelay",type=float,default=.5)
+    parser_interactions.add_argument("-w","--phage_delaydistr",type=float,default=.05)
+    parser_interactions.add_argument("-L","--phage_burstsize_linear",type=float,default=100)
+    parser_interactions.add_argument("-l","--phage_burstsize_const",type=float,default=2)
+    parser_interactions.add_argument("-A","--phage_bacteria_absorptionconstant",type=float,default=1e-4)
+    parser_interactions.add_argument("-m","--bacteria_growth_Kmax",type=float,default=0.7204)
+    parser_interactions.add_argument("-c","--bacteria_growth_Kc",type=float,default=0.0000257024)
+    parser_interactions.add_argument("-n","--nutrients_per_cell",type=float,default=1e-10)          # mg/cell
+    parser_interactions.add_argument("-r","--bacteria_resitant_absorptionreduction",type=float,default=1e-3)
+
+    # startconcentrations 
+    parser_startconc = parser.add_argument_group(description="Start concentrations")
+    parser_startconc.add_argument("-S","--startconcentrationsusceptibles",type=float,default=10e4) # Indiv/mm
+    parser_startconc.add_argument("-R","--startconcentrationresistant",   type=float,default=0e4)  # Indiv/mm
+    parser_startconc.add_argument("-P","--startconcentrationphages",      type=float,default=100)  # Indiv in first bin
+    parser_startconc.add_argument("-N","--startconcentrationnutrients",   type=float,default=1e-4) # mg/mm
+    parser_startconc.add_argument("-p","--initialplaquesize",             type=float,default=.5)   # radius in mm
+
+
+    global args
+    args = parser.parse_args()
+    
+    # array with all concentrations/numbers of cells
+    y = np.zeros((6,args.space))
+
+    # store solutions to implement time-delay in phage burst
+    delaysize = int((args.phage_burstdelay+3*args.phage_delaydistr)/args.epsilon)
+    delaytime = np.arange(delaysize)*args.epsilon-3*args.phage_delaydistr
+    # two distributions shifted by a timestep for use in Runge-Kutta algorithm
+    delaydistr1 = np.exp(-0.5*(delaytime/args.phage_delaydistr)**2)/np.sqrt(2*math.pi*args.phage_delaydistr**2)*args.epsilon
+    delaydistr2 = np.exp(-0.5*((delaytime+args.epsilon)/args.phage_delaydistr)**2)/np.sqrt(2*math.pi*args.phage_delaydistr**2)*args.epsilon
+    
+    sdelay = np.zeros((delaysize,args.space))
+    rdelay = np.zeros((delaysize,args.space))
+    
+    y[0,:] = args.startconcentrationnutrients
+    # not concentrations, but rather numbers of cells/phages in a given bin
+    y[1,:] = args.startconcentrationsusceptibles
+    y[2,:] = 0
+    y[3,:] = args.startconcentrationresistant
+    y[4,:] = 0
+    y[5,:int(args.initialplaquesize/args.dx)] = args.startconcentrationphages #*np.exp(-i*args.dx/args.startdecayrate)
+
+    for o in range(args.maxsteps):
+	# delay as convolution with 
+	yd = np.array([[np.dot(delaydistr1,sdelay),np.dot(delaydistr1,rdelay)],[np.dot(delaydistr2,sdelay),np.dot(delaydistr2,rdelay)]])
+	
+	# integrate system of differential equations with Runge-Kutta method
+	# should be more stable than naive Euler integration
+	y = RungeKutta4( y ,yd)
+ 	
+	# update stored solutions:
+	# drop first item (0 as python index), append new solutions from current timestep to the end
+	sdelay[0:delaysize-1,:] = sdelay[1:delaysize,:]
+	sdelay[delaysize-1,:] = y[1]*y[5]
+	rdelay[0:delaysize-1,:] = rdelay[1:delaysize,:]
+	rdelay[delaysize-1,:] = y[3]*y[5]
+
+	## define front as steepest point in the profile of phage concentration
+	#xf1 = (np.diff(y[4])).argmin()*args.dx
+	## define front as first bin with a phage in it
+	#if len(y[4][y[4]>1]) > 0:
+	    #xf2 = (y[4][y[4]>1]).argmax()*args.dx
+	#else: # phages extinct
+	    #xf2 = 0
+	
+	if o%args.outputsteps == 0:
+	    # integrate profiles to get total number of ...
+	    nn = np.sum(y[0])*args.dx          # nutrients
+	    ss = np.sum(y[1])*args.dx          # susceptibles
+	    si = np.sum(y[2])*args.dx          # infected
+	    rr = np.sum(y[3])*args.dx          # resistant
+	    ri = np.sum(y[4])*args.dx          # res inf
+	    pp = np.sum(y[5])*args.dx          # phages
+	    print "%10.4lf %.10e %.10e %.10e %.10e %.10e %.10e"%(o*args.epsilon,nn,ss,si,rr,ri,pp)
+	    
+	    if not args.quiet:
+		for i in range(0,args.space,args.outputbins):
+		    print >> sys.stderr,"%lf %lf %.10e %.10e %.10e %.10e %.10e %.10e"%(o*args.epsilon,i*args.dx,y[0,i],y[1,i],y[2,i],y[3,i],y[4,i],y[5,i])
+		print >> sys.stderr
+
+
+# more "pythonian" version of the program:
+# whole file can be loaded as module, and then be executed as function call to "main()" (as defined above)
+# if executed on its own, then python automatically goes here (and runs "main()")
+if __name__=="__main__":
+  main()
+  
+  
+  
+
+  
--- rendermovie.py
+++ rendermovie.py
@@ -1,1 +1,109 @@
-/home/lukas/projects/phagegrowth/program/rendermovie.py
+#!/usr/bin/env python
+
+import numpy as np
+import argparse
+import sys,math
+import Gnuplot
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c","--conffile",required=True)
+parser.add_argument("-N","--normalize",action='store_true',default=False)
+parser.add_argument("-m","--maxlength",type=float,default=None)
+args = parser.parse_args()
+
+try:
+    data = np.genfromtxt(args.conffile)
+except:
+    print >> sys.stderr,"could not open file"
+    exit(1)
+
+tall  = data[:,0]
+xall  = data[:,1]
+nall  = data[:,2]
+sall  = data[:,3]
+siall = data[:,4]
+rall  = data[:,5]
+riall = data[:,6]
+pall  = data[:,7]
+
+tlist = np.unique(tall)
+
+dt = tall[1] - tlist[0]
+
+x = xall[tall == tall[1]]
+if args.maxlength == None:
+    xmax = x[-1]
+else:
+    xmax = args.maxlength
+
+
+m = np.ones(6)
+if args.normalize:
+    m[0] = max(nall)
+    m[1] = max(sall)
+    m[2] = max(siall)
+    m[3] = max(rall)
+    m[4] = max(riall)
+    m[5] = max(pall)
+
+frame = 0
+
+
+print m
+#exit(1)
+
+g = Gnuplot.Gnuplot(persist=0)
+
+
+for t in tlist:
+    print frame, t
+    
+    n = nall[tall==t]/m[0]
+    s = sall[tall==t]/m[1]
+    si = siall[tall==t]/m[2]
+    r = rall[tall==t]/m[3]
+    ri = riall[tall==t]/m[4]
+    p = pall[tall==t]/m[5]
+    
+    #print x,n,s
+
+    
+
+
+    pn  = Gnuplot.Data(x,n ,with_="lines lw 2 lc rgb \"#f57900\"", title="N(t,x)")
+    ps  = Gnuplot.Data(x,s ,with_="lines lw 2 lc rgb \"#204a87\"", title="S(t,x)")
+    psi = Gnuplot.Data(x,si,with_="lines lw 2 lc rgb \"#729fcf\"", title="SI(t,x)")
+    pr  = Gnuplot.Data(x,r ,with_="lines lw 2 lc rgb \"#4e9a06\"", title="R(t,x)")
+    pri = Gnuplot.Data(x,ri,with_="lines lw 2 lc rgb \"#8ae234\"", title="RI(t,x)")
+    pp  = Gnuplot.Data(x,p ,with_="lines lw 4 lc rgb \"#ef2929\"", title="P(t,x)")
+
+    g.reset()
+    
+    g("set terminal pngcairo enhanced size 1000,400")
+    g("set output \"frame%04d.png\""%frame)
+    g("unset key")
+    g("set border 15 lw 2")
+    g("set tics front")
+    g("set key bottom right spacing 2")
+    frame += 1
+    
+    g("set xra [0:%lf]"%(xmax))
+    #g("set xtics 500")
+    
+    if args.normalize:
+        g("set yra [0:1.1]")
+        g("set ytics (.5,\"max\"1)")
+    else:
+        g("set yra [0:%lf]"%1.1*max(m))
+    
+    g("set label 1 \"t = %.1lf\" at screen .25,.9 center front"%t)
+    
+    g.plot(pn,ps,psi,pr,pri,pp)
+    
+    del pn
+    del ps
+    del pr
+    del pp
+    del psi
+    del pri
+
