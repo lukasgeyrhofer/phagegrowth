06c63d493cfa90afe02357fc005d86f01151c13c
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-06-20 18:40:46 +0300

    typos

--- phagedynamics_withoutspace.py
+++ phagedynamics_withoutspace.py
@@ -12,6 +12,8 @@ def RungeKutta4(func,y,epsilon):
   k3 = epsilon*func(y+k2/2.)
   k4 = epsilon*func(y+k3)
   ret = y + (k1+2*k2+2*k3+k4)/6.
+  ret[ret<0] = 0.
+  return ret
 
 
 def phagedynamics(y):
@@ -35,7 +37,7 @@ def main():
     parser.add_argument("-a","--param_growthrate",type=float,default=.7)
     parser.add_argument("-b","--param_burstsize",type=float,default=100)
     parser.add_argument("-n","--param_absorption",type=float,default=1e-4)
-    parser.add_argument("-y","--param_yield",type=float,default=1e-3)
+    parser.add_argument("-y","--param_yield",type=float,default=1e3)
     
     parser.add_argument("-e","--algorithm_epsilon",type=float,default=1e-3)
     parser.add_argument("-m","--algorithm_maxtime",type=float,default=10)
@@ -49,9 +51,11 @@ def main():
     y = np.array([args.initial_bacteria,args.initial_susceptible_fraction, args.initial_phage, args.initial_nutrients])
     
     i = 0
-    while t < args.algorithm_maxtime:
+    maxsteps = args.algorithm_maxtime / args.algorithm_epsilon
+    while i < maxsteps:
         y = RungeKutta4(phagedynamics,y,args.algorithm_epsilon)
         i += 1
+
         if i % args.algorithm_outputstep == 0:
             print "{:.2f} {:.6e} {:.6e} {:.6e} {:.6e}".format(i*args.algorithm_epsilon, y[0],y[1],y[2],y[3])
 
