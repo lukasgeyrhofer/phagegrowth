df4eb7b6aedfab34d37c73377ff1a4221caddc96
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2016-06-21 23:43:53 +0300

    small changes

--- phagedynamics_withoutspace.py
+++ phagedynamics_withoutspace.py
@@ -25,28 +25,28 @@ def phagedynamics(y):
     return np.array([ (growthrate - y[1]*y[2]*param['absorption'])*y[0],
                      -param['absorption']*y[1]*(1-y[1])*y[2],
                      (param['burstsize']*y[1]-1)*param['absorption']*y[0]*y[2],
-                     -growthrate/param['yield']*y[0]])
+                     -growthrate*   param['invyield']*y[0]])
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-B","--initial_bacteria",type=float,default=1e5)
     parser.add_argument("-S","--initial_susceptible_fraction",type=float,default=.5)
     parser.add_argument("-P","--initial_phage",type=float,default=1e1)
-    parser.add_argument("-N","--initial_nutrients",type=float,default=1)
+    parser.add_argument("-N","--initial_nutrients",type=float,default=1e-3)
     
     parser.add_argument("-a","--param_growthrate",type=float,default=.7)
     parser.add_argument("-b","--param_burstsize",type=float,default=100)
     parser.add_argument("-n","--param_absorption",type=float,default=1e-4)
-    parser.add_argument("-y","--param_yield",type=float,default=1e3)
+    parser.add_argument("-y","--param_nutrientpercell",type=float,default=1e-9)
     
     parser.add_argument("-e","--algorithm_epsilon",type=float,default=1e-3)
-    parser.add_argument("-m","--algorithm_maxtime",type=float,default=10)
+    parser.add_argument("-T","--algorithm_maxtime",type=float,default=10)
     parser.add_argument("-O","--algorithm_outputstep",type=int,default=100)
     
     args = parser.parse_args()
     
     global param
-    param = {'growthrate' : args.param_growthrate, 'burstsize': args.param_burstsize, 'absorption': args.param_absorption, 'yield': args.param_yield}
+    param = {'growthrate' : args.param_growthrate, 'burstsize': args.param_burstsize, 'absorption': args.param_absorption, 'invyield': args.param_nutrientpercell}
     
     y = np.array([args.initial_bacteria,args.initial_susceptible_fraction, args.initial_phage, args.initial_nutrients])
     
@@ -57,7 +57,7 @@ def main():
         i += 1
 
         if i % args.algorithm_outputstep == 0:
-            print "{:.2f} {:.6e} {:.6e} {:.6e} {:.6e}".format(i*args.algorithm_epsilon, y[0],y[1],y[2],y[3])
+            print "{:.4f} {:.6e} {:.6e} {:.6e} {:.6e}".format(i*args.algorithm_epsilon, y[0],y[1],y[2],y[3])
 
 
 if __name__ == "__main__":
