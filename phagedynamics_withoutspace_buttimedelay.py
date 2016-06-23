1690efb82ed29429b8af67d0120286caa5d87734
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-06-23 12:20:48 +0300

    added infected populations back

--- phagedynamics_withoutspace_buttimedelay.py
+++ phagedynamics_withoutspace_buttimedelay.py
@@ -5,32 +5,36 @@ import argparse
 import sys,math
 
 
-def RungeKutta4(func,y,epsilon):
+def RungeKutta4(func,y,yd,epsilon):
   # 4th order Runge-Kutta integration scheme
-  k1 = epsilon*func(y)
-  k2 = epsilon*func(y+k1/2.)
-  k3 = epsilon*func(y+k2/2.)
-  k4 = epsilon*func(y+k3)
+  k1 = epsilon*func(y,yd[0])
+  k2 = epsilon*func(y+k1/2.,np.mean(yd,axis=1))
+  k3 = epsilon*func(y+k2/2.,np.mean(yd,axis=1))
+  k4 = epsilon*func(y+k3,yd[1])
   ret = y + (k1+2*k2+2*k3+k4)/6.
   ret[ret<0] = 0.
   return ret
 
 
-def phagedynamics(y):
-    if y[3] <= 0:
-        growthrate = 0
-    else:
-        growthrate = param['growthrate']
+def phagedynamics(y,yd):
+    # monod kinetics for bacterial growth
+    growthrate = param['growthrate'] * y[5]/(param['growthkm'] + y[5])
+    # define burstsize as compound parameter for phage growth, depends linearly on bacterial growthrate and interpolates between minimal and maximal burstsizes
+    burstsize = growthrate/param['growthrate'] * ( param['phagemaxburst'] - param['phageminburst']) + param['phageminburst']
+    
+    return np.array([growthrate * y[0] - param['absorption']*y[0]*y[4],                                                     # 0: susceptible bacteria
+                     growthrate * y[1],                                                                                     # 1: resistant bacteria
+                     param['absorption'] * y[0] * y[4] - yd/param['timedelay_mean'],                                        # 2: susceptible bacteria infected
+                     0,                                                                                                     # 3: resistant bacteria infected
+                     burstsize * yd/param['timedelay_mean'] - param['absorption'] * (y[0] + y[1] + y[2] + y[3]) * y[4],     # 4: phage
+                     -growthrate*param['invyield']*(y[0] + y[1] + y[2] + y[3]) ])                                           # 5: nutrients
+    
         
-    return np.array([ (growthrate - y[1]*y[2]*param['absorption'])*y[0],
-                     -param['absorption']*y[1]*(1-y[1])*y[2],
-                     (param['burstsize']*y[1]-1)*param['absorption']*y[0]*y[2],
-                     -growthrate*   param['invyield']*y[0]])
-
+        
 def output(time,concentrations,widthtime = 4):
     print "{value:.{width}f}".format(value=time,width = widthtime),
     for c in concentrations:
-        print "{:.5e}".format(c),
+        print "{:12.5e}".format(c),
     print
 
 def main():
@@ -43,38 +47,50 @@ def main():
     parser.add_argument("-a","--param_growthrate",type=float,default=.7)
     parser.add_argument("-b","--param_burstsize",type=float,default=100)
     parser.add_argument("-n","--param_absorption",type=float,default=1e-4)
-    parser.add_argument("-y","--param_nutrientpercell",type=float,default=1e-11)
+    parser.add_argument("-y","--param_nutrientpercell",type=float,default=1e-9)
+    parser.add_argument("-k","--param_growth_km",type=float,default = 0.0000257024)
+    parser.add_argument("-g","--param_minburst",type=float,default=2)
+    parser.add_argument("-G","--param_maxburst",type=float,default=100)
     
     parser.add_argument("-t","--timedelay_mean",type=float,default=0.5)     # latent period is half hour
     parser.add_argument("-s","--timedelay_stddev",type=float,default=0.08)  # with a standard deviation of roughly 5min
     
+    
     parser.add_argument("-e","--algorithm_epsilon",type=float,default=1e-3)
-    parser.add_argument("-T","--algorithm_maxtime",type=float,default=10)
+    parser.add_argument("-T","--algorithm_maxtime",type=float,default=20)   # in hrs
     parser.add_argument("-O","--algorithm_outputstep",type=int,default=100)
     
     args = parser.parse_args()
-    
+
     global param
-    param = {'growthrate' : args.param_growthrate, 'burstsize': args.param_burstsize, 'absorption': args.param_absorption, 'invyield': args.param_nutrientpercell}
+    param = {'growthrate' : args.param_growthrate, 'burstsize': args.param_burstsize, 'absorption': args.param_absorption, 'invyield': args.param_nutrientpercell, 'growthkm': args.param_growth_km, 'phageminburst': args.param_minburst, 'phagemaxburst':args.param_maxburst, 'timedelay_mean' : args.timedelay_mean, 'timedelay_stddev' : args.timedelay_stddev}
+    outputwidth = int(np.rint(-np.log10(args.algorithm_epsilon * args.algorithm_outputstep)))
+    maxsteps = int(args.algorithm_maxtime / args.algorithm_epsilon)
+    
 
     # store solutions to implement time-delay in phage burst
-    delaysize = int((args.phage_burstdelay+3*args.phage_delaydistr)/args.epsilon)
-    delaytime = np.arange(delaysize)*args.epsilon-3*args.phage_delaydistr
+    delaysize = int((args.timedelay_mean+3*args.timedelay_stddev)/args.algorithm_epsilon)
+    delaytime = np.arange(delaysize)*args.algorithm_epsilon-3*args.timedelay_stddev
     # two distributions shifted by a timestep for use in Runge-Kutta algorithm
-    delaydistr1 = np.exp(-0.5*(delaytime/args.phage_delaydistr)**2)/np.sqrt(2*math.pi*args.phage_delaydistr**2)*args.epsilon
-    delaydistr2 = np.exp(-0.5*((delaytime+args.epsilon)/args.phage_delaydistr)**2)/np.sqrt(2*math.pi*args.phage_delaydistr**2)*args.epsilon
+    delaydistr1 = np.exp(-0.5*(delaytime/args.timedelay_stddev)**2)/np.sqrt(2*math.pi*args.timedelay_stddev**2)*args.algorithm_epsilon
+    delaydistr2 = np.exp(-0.5*((delaytime+args.algorithm_epsilon)/args.timedelay_stddev)**2)/np.sqrt(2*math.pi*args.timedelay_stddev**2)*args.algorithm_epsilon
     
-    y = np.array([args.initial_bacteria,args.initial_susceptible_fraction, args.initial_phage, args.initial_nutrients])
+    # generate initial conditions
+    y = np.array([args.initial_bacteria * args.initial_susceptible_fraction, args.initial_bacteria* (1. - args.initial_susceptible_fraction), 0., 0., args.initial_phage, args.initial_nutrients])
+    infecteddelay = np.zeros((delaysize,2))
+    yd = np.array([np.dot(delaydistr1,infecteddelay),np.dot(delaydistr2,infecteddelay)])
     
-    i = 0
-    maxsteps = args.algorithm_maxtime / args.algorithm_epsilon
-    outputwidth = int(np.rint(-np.log10(args.algorithm_epsilon * args.algorithm_outputstep)))
-    print outputwidth
     output(0,y,outputwidth)
-    while i < maxsteps:
-        y = RungeKutta4(phagedynamics,y,args.algorithm_epsilon)
-        i += 1
 
+    for i in range(1,maxsteps+1):
+
+        infecteddelay[0:delaysize-1,:] = infecteddelay[1:delaysize,:]
+        infecteddelay[delaysize-1,0] = y[2]
+        infecteddelay[delaysize-1,1] = y[3]
+        yd = np.array([np.dot(delaydistr1,infecteddelay,axis=0),np.dot(delaydistr2,infecteddelay,axis=0)])
+
+        y = RungeKutta4(phagedynamics,y,yd,args.algorithm_epsilon)
+        
         if i % args.algorithm_outputstep == 0:
             output(i*args.algorithm_epsilon,y,outputwidth)
 
