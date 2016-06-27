4005616e3a0e4415cbc351722bf02df0b4c1dc23
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-06-27 11:20:05 +0300

    more methods to estimate front

--- getFront.py
+++ getFront.py
@@ -6,6 +6,7 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infile")
+parser.add_argument("-c","--plaquevisibilitythreshold",type=float,default=1e6)
 args = parser.parse_args()
 
 try:
@@ -17,7 +18,7 @@ except:
 timetrace = data[:,0]
 timesteps = np.unique(timetrace)
 
-methods = ['avginf','maxinf','avgphage','maxphage']
+methods = ['avginf','maxinf','avgphage','maxphage','bact1','bact2','visibility']
 
 for t in timesteps:
     conf = data[timetrace == t]
@@ -35,6 +36,30 @@ for t in timesteps:
     pos['avgphage'] = np.dot(p,x)/np.sum(p)
     pos['maxinf']   = x[(i0+i1).argmax()]
     pos['maxphage'] = x[p.argmax()]
+    
+    pos['bact1']    = x[np.diff(b0,n=1).argmax()]
+    pos['bact2']    = x[np.diff(b0,n=2).argmin()]
+    
+    allbact = b0 + i0 + b1 + i0 - args.plaquevisibilitythreshold
+    if 0 < len(allbact[allbact < 0]) < len(allbact):
+        idx1 = (allbact**2).argmin()
+        if allbact[idx1] < 0:
+            idxbelow = idx1
+            if allbact[idx1+1] < 0:
+                idxupper = idx1 - 1
+            else:
+                idxupper = idx1 + 1
+        else:
+            idxupper = idx1
+            if allbact[idx1+1] < 0:
+                idxbelow = idx1 + 1
+            else:
+                idxbelow = idx1 - 1
+    try:
+        pos['visibility'] = (x[idxupper] * allbact[idxupper] - x[idxbelow] * allbact[idxbelow])/(allbact[idxupper] - allbact[idxbelow])
+    except:
+        pos['visibility'] = 0
+    
     
     print "{:.3f}".format(t),
     for k in methods:
