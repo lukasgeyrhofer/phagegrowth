dcd14e1bc66555dc8dafeb1949b81a37a839aed0
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2016-07-18 00:18:20 +0300

    estimate phage growth rate

--- extractexperimentaldata/getAbsorptionConstant.py
+++ extractexperimentaldata/getAbsorptionConstant.py
@@ -6,17 +6,27 @@ import sys
 from scipy.optimize import curve_fit
 
 def radius(s,delta):
-    return 2*np.sqrt(params['diffusionconstant'] * delta * (params['burstsize']/params['latentperiod']*s - 1))/params['bacterialgrowthrate']*np.log(params['bacterialgrowthratio'])
+    herdimmunity = params['burstsizeoverlatentperiod']*s - 1
+    herdimmunity[herdimmunity < 0] =0
+    return 2*np.sqrt(params['diffusionconstant'] * delta * herdimmunity)/params['bacterialgrowthrate']*np.log(params['bacterialgrowthratio'])
 
+
+def radius2(s,delta,betaoverlambda):
+    herdimmunity = betaoverlambda*s - 1
+    herdimmunity[herdimmunity < 0] =0
+    return 2*np.sqrt(params['diffusionconstant'] * delta * herdimmunity)/params['bacterialgrowthrate']*np.log(params['bacterialgrowthratio'])
+    
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infile")
 parser.add_argument("-D","--diffusionconstant",type=float,default=1.17e-2)
-parser.add_argument("-b","--burstsize",type=float,default=91)
-parser.add_argument("-l","--latentperiod",type=float,default=.5)
+parserBL = parser.add_mutually_exclusive_group()
+parserBL.add_argument("-E","--estimatebetaoverlambda",default=False,action="store_true")
+parserBL.add_argument("-b","--burstsizeoverlatentperiod",type=float,default=182.)
 parser.add_argument("-a","--bacterialgrowthrate",type=float,default=.72)
 parser.add_argument("-B","--bacterialgrowthratio",type=float,default=1e3)
 parser.add_argument("-C","--excludecontrol",action="store_true",default=False)
+parser.add_argument("-m","--maxfev",default=2000,type=int)
 args = parser.parse_args()
 
 
@@ -32,27 +42,48 @@ params = vars(args)
 
 s = data[:,0]
 r = data[:,1]
+try:
+    e = data[:,2]
+except:
+    pass
 
 if args.excludecontrol:
     s = s[s<1]
     r = r[s<1]
 
 
-deltafit,deltacov = curve_fit(radius,s,r)
+if args.estimatebetaoverlambda:
+    initialguess = np.array([1e-2,2e2])
+    paramfit,paramcov = curve_fit(radius2,s,r,p0 = initialguess,maxfev = args.maxfev)
+else:
+    initialguess = np.array([1e-2])
+    paramfit,paramcov = curve_fit(radius,s,r,p0 = initialguess,maxfev = args.maxfev)
 
 print "# ************************************************************ #"
 print "#   estimate absorption constant delta for phage on bacteria   #"
 print "# ************************************************************ #"
-print "# delta = {:.10e}".format(deltafit[0])
-print "# deltastddev = {:.10e}".format(np.sqrt(deltacov[0][0]))
+print "# delta          = {:.10e}".format(paramfit[0])
+print "# delta stddev   = {:.10e}".format(np.sqrt(paramcov[0][0]))
+if args.estimatebetaoverlambda:
+    print "# betaoverlambda = {:.10e}".format(paramfit[1])
+    print "# bl stddev      = {:.10e}".format(np.sqrt(paramcov[1][1]))
+    params['burstsizeoverlatentperiod'] = paramfit[1]
+    predictedradius = radius2(s,paramfit[0],paramfit[1])
+else:
+    predictedradius = radius(s,paramfit[0])
 print "# ************************************************************ #"
 for key,value in params.iteritems():
     print "# {} = {}".format(key,value)
-print "# diameter(s) = 4*sqrt({:.10e} * {:.10e} * (({:.10e}/{:.10e} + 1)*s -1 ))/{:.10e}*log({:.10e})".format(params['diffusionconstant'],deltafit[0],params['burstsize'],params['latentperiod'],params['bacterialgrowthrate'],params['bacterialgrowthratio'])
+print "# radius(s) = 2*sqrt({:.10e} * {:.10e} * ({:.10e}*s -1 ))/{:.10e}*log({:.10e})".format(params['diffusionconstant'],paramfit[0],params['burstsizeoverlatentperiod'],params['bacterialgrowthrate'],params['bacterialgrowthratio'])
 print "# ************************************************************ #"
 print "#   data                                                       #"
 print "# ************************************************************ #"
 for i in range(len(s)):
-    print "{:.2f} {:.10f} {:.10f}".format(s[i],radius(s[i],deltafit[0]),r[i])
+    print "{:.2f} {:.10f} {:.10f}".format(s[i],predictedradius[i],r[i]),
+    try:
+        print "{:.10f}".format(e[i]),
+    except:
+        pass
+    print
 
 
